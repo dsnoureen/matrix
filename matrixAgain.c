a75d7e46a9e497fa0f1aec0475970a790b088634
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-28 19:37:38 +0600

    modifying

---
+++ matrixAgain.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int arr[n][m];
+    int flag = 1;
+    if (n != m)
+    {
+        flag = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int i = n - 1;
+    for (int j = 0; j < m; j++)
+    {
+        printf("%d ", arr[i][j]);
+    }
+    printf("\n");
+    int j = n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i][j]);
+    }
+    return 0;
+}
