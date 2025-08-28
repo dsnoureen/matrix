9cc0573ac1c596b1d281a6f3a1229e7b005e9696
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-28 19:22:59 +0600

    jadu matrix done

---
+++ jaduMatrix.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int arr[n][m];
+    int element = n * m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int flag = 1;
+    
+    if (n != m)
+    {
+        flag=0;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (i == j || i+j == n - 1)
+            {
+                if (arr[i][j] == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+            else if (arr[i][j] != 0)
+            {
+                flag = 0;
+            }
+        }
+    }
+
+    if (flag == 1)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
